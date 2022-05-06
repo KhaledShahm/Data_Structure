018b1e0a7a27a36f0dee2c3b53283abe12560cae
Khaled Shahm <50189742+KhaledShahm@users.noreply.github.com>
2022-05-07 01:41:31 +0200

    updated_string

--- Arrays and Linked Lists/string.py
+++ Arrays and Linked Lists/string.py
@@ -1,76 +1,90 @@
-# # 1-Reverse String 
-# def ReveseString(our_string):
-#     """
-#     Reverse the input string 
-#     Args:
-#         our_string(string): String to be reversed
-#     Returns:
-#         string: the reversed string 
-#     """
-#     #TODO: Write your solution here    
-#     reverse_string = ""
-#     for i in range(len(our_string)):
-#         reverse_string += our_string[(len(our_string)-1)-i]
+def string_reverser(our_string):
+
+    """
+    Reverse the input string
+
+    Args:
+       our_string(string): String to be reversed
+    Returns:
+       string: The reversed string
+    """
     
-#     return reverse_string
+    # TODO: Write your solution here
+    out_string = '' # Stored reversed string in out_string
+    for i in range (len(our_string)):
+        out_string += our_string[(len(our_string)-1) - i]
 
-# # Test cases
-# print("Pass" if("retaw" == ReveseString("water")) else "Fail")
-# print ("Pass" if ('!noitalupinam gnirts gnicitcarP' == ReveseString('Practicing string manipulation!')) else "Fail")
-# print ("Pass" if ('3432 :si edoc esuoh ehT' == ReveseString('The house code is: 2343')) else "Fail")
+    return out_string
+
+""" 
+print ("Pass" if ('retaw' == string_reverser('water')) else "Fail")
+print ("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
+print ("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
+"""
+
+# Code
+
+def anagram_checker(str1, str2):
+
+    """
+    Check if the input strings are anagrams of each other
+
+    Args:
+       str1(string),str2(string): Strings to be checked
+    Returns:
+       bool: Indicates whether strings are anagrams
+    """
+    
+    # TODO: Write your solution here
+ 
+    # Clean strings 
+    clean_str1 = str1.replace(" ", "").lower()
+    clean_str2 = str2.replace(" ", "").lower()
+    if sorted(clean_str2) == sorted(clean_str1):
+        return True
+
+    return False
+
+""" 
+print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
+print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
+print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
+print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
+print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")
+"""
+
+# Code 
+
+def word_flipper(our_string):
+
+    """
+    Flip the individual words in a sentence
+
+    Args:
+       our_string(string): String with words to flip
+    Returns:
+       string: String with words flipped
+    """
+    
+    # TODO: Write your solution here
+    word_list = our_string.split(" ")
+    for index in range (len(word_list)):
+        word_list[index] = word_list[::-1]
+    
+    return " ".join(word_list)
+
+"""  
+
+# Test Cases
+
+print ("Pass" if ('retaw' == word_flipper('water')) else "Fail")
+print ("Pass" if ('sihT si na elpmaxe' == word_flipper('This is an example')) else "Fail")
+print ("Pass" if ('sihT si eno llams pets rof ...' == word_flipper('This is one small step for ...')) else "Fail")
+
+"""
 
 
-
-# # 2-Anagrams
-# def AnagramChecker(str1, str2):
-#     """
-#     check if the input strings are anagrams of each other
-#     Args:
-#         str1(string), str2(string) : strings to be checked
-#     Returns:
-#         bool: Indicates whether strings are anagrams
-#     """
-#     if len(str1) != len(str2):
-
-#         # Clean strings
-#         clean_str_1 = str1.replace(" ", "").lower()
-#         clean_str_2 = str2.replace(" ", "").lower()
-
-#         if sorted(clean_str_2) == sorted(clean_str_1):
-#             return True
-#     return False
-
-# print ("Pass" if not (AnagramChecker('water','waiter')) else "Fail")
-# print ("Pass" if AnagramChecker('Dormitory','Dirty room') else "Fail")
-# print ("Pass" if AnagramChecker('Slot machines', 'Cash lost in me') else "Fail")
-# print ("Pass" if not (AnagramChecker('A gentleman','Elegant men')) else "Fail")
-# print ("Pass" if AnagramChecker('Time and tide wait for no man','Notified madman into water') else "Fail")
-
-
-# # 3- Word flipper
-# def FlipWord(our_string):
-
-#     """
-#     Flip the individual words in a sentence
-
-#     Args:
-#        our_string(string): Strings to have individual words flip
-#     Returns:
-#        string: String with words flipped
-#     """
-
-#     word_list = our_string.split(" ")
-
-#     for idx in range(len(word_list)):
-#         word_list[idx] = word_list[idx][::-1]
-
-#     return " ".join(word_list)
-
-
-# print ("Pass" if ('retaw' == FlipWord('water')) else "Fail")
-# print ("Pass" if ('sihT si na elpmaxe' == FlipWord('This is an example')) else "Fail")
-# print ("Pass" if ('sihT si eno llams pets rof ...' == FlipWord('This is one small step for ...')) else "Fail")
-
+# Code
 
 def hamming_distance(str1, str2):
 
@@ -82,23 +96,18 @@ def hamming_distance(str1, str2):
     Returns:
        int: Hamming Distance
     """
-
+    
+    # TODO: Write your solution here
     if len(str1) == len(str2):
         count = 0
-
         for char in range(len(str1)):
             if str1[char] != str2[char]:
                 count+=1
-
         return count
-
     return None
-
-
 
 print ("Pass" if (10 == hamming_distance('ACTTGACCGGG','GATCCGGTACA')) else "Fail")
 print ("Pass" if  (1 == hamming_distance('shove','stove')) else "Fail")
 print ("Pass" if  (None == hamming_distance('Slot machines', 'Cash lost in me')) else "Fail")
 print ("Pass" if  (9 == hamming_distance('A gentleman','Elegant men')) else "Fail")
 print ("Pass" if  (2 == hamming_distance('0101010100011101','0101010100010001')) else "Fail")
-
